6f85adba737e9af442fbea819494d1440bfb9f7a
kankodakoda <sebastian@wernqvist.eu>
2025-02-04 08:52:32 +0100

    Add Scania and Truck

---
+++ Truck.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public abstract class Truck extends Car {
+    
+    // variables
+
+    public Truck(String modelName, int nrDoors, double enginePower, Color color, double xPosition, double yPosition) {
+        super(modelName, nrDoors, enginePower, color, xPosition, yPosition);
+    }
+
+    //  methods
+    
+
+        double rampPosition;
+    double 
+
+}
